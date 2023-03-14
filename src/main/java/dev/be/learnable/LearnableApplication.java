119abeae038440b4b2e7a6bfc710622fc6417598
DoooongDong <rhkswls98@kakao.com>
2023-03-14 23:25:07 +0900

    first commit

---
+++ src/main/java/dev/be/learnable/LearnableApplication.java
@@ -0,0 +1,13 @@
+package dev.be.learnable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnableApplication.class, args);
+    }
+
+}
