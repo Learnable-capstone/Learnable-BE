74db85f0109fa2168809098c51a5b7828b1b7294
DoooongDong <rhkswls98@kakao.com>
2023-03-29 00:46:34 +0900

    feature: [#15] 채팅방 서비스 코드 작성

---
+++ src/main/java/dev/be/learnable/core/service/ChatRoomService.java
@@ -0,0 +1,31 @@
+package dev.be.learnable.core.service;
+
+import dev.be.learnable.core.domain.ChatRoom;
+import dev.be.learnable.core.domain.Member;
+import dev.be.learnable.core.domain.Subject;
+import dev.be.learnable.core.dto.ChatRoomDto;
+import dev.be.learnable.core.repository.ChatRoomRepository;
+import dev.be.learnable.core.repository.MemberRepository;
+import dev.be.learnable.core.repository.SubjectRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ChatRoomService {
+
+    private final ChatRoomRepository chatRoomRepository;
+    private final MemberRepository memberRepository;
+    private final SubjectRepository subjectRepository;
+
+    @Transactional
+    public void createChatRoom(ChatRoomDto chatRoomDto) {
+        Member member = memberRepository.getReferenceById(chatRoomDto.getMemberId());
+        Subject subject = subjectRepository.getReferenceById(chatRoomDto.getSubjectId());
+        chatRoomRepository.save(chatRoomDto.toEntity(member, subject));
+    }
+}
