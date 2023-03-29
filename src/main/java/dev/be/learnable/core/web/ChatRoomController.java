73353b326277a899327fc21b640323fe46b5d996
DoooongDong <rhkswls98@kakao.com>
2023-03-30 01:44:53 +0900

    feature: [#15] 채팅방 전체 조회 컨트롤러 및 테스트 코드 작성

--- src/main/java/dev/be/learnable/core/web/ChatRoomController.java
+++ src/main/java/dev/be/learnable/core/web/ChatRoomController.java
@@ -1,11 +1,17 @@
 package dev.be.learnable.core.web;
 
 import static dev.be.learnable.common.response.ResponseCodeAndMessages.CREATE_CHAT_ROOM_SUCCESS;
+import static dev.be.learnable.common.response.ResponseCodeAndMessages.FIND_ALL_CHAT_ROOM_SUCCESS;
 
 import dev.be.learnable.common.response.BaseResponse;
+import dev.be.learnable.core.dto.ChatRoomDto;
 import dev.be.learnable.core.dto.request.ChatRoomRequest;
+import dev.be.learnable.core.dto.response.ChatRoomResponse;
+import dev.be.learnable.core.dto.response.ChatRoomsResponse;
 import dev.be.learnable.core.service.ChatRoomService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +29,12 @@ public class ChatRoomController {
     ) {
         chatRoomService.create(chatRoomRequest.toDto());
         return new BaseResponse<>(CREATE_CHAT_ROOM_SUCCESS, null);
+    }
+
+    @GetMapping
+    public BaseResponse<ChatRoomsResponse> findAll(Long memberId) {
+        List<ChatRoomResponse> chatRoomResponses = chatRoomService.findAll(memberId);
+        ChatRoomsResponse response = ChatRoomsResponse.of(chatRoomResponses);
+        return new BaseResponse<>(FIND_ALL_CHAT_ROOM_SUCCESS, response);
     }
 }
--- src/test/java/dev/be/learnable/core/web/ChatRoomControllerTest.java
+++ src/test/java/dev/be/learnable/core/web/ChatRoomControllerTest.java
@@ -1,10 +1,14 @@
 package dev.be.learnable.core.web;
 
 import static dev.be.learnable.common.response.ResponseCodeAndMessages.CREATE_CHAT_ROOM_SUCCESS;
+import static dev.be.learnable.common.response.ResponseCodeAndMessages.FIND_ALL_CHAT_ROOM_SUCCESS;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.BDDMockito.willDoNothing;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -13,13 +17,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.be.learnable.common.response.BaseResponse;
 import dev.be.learnable.core.dto.ChatRoomDto;
 import dev.be.learnable.core.dto.request.ChatRoomRequest;
+import dev.be.learnable.core.dto.response.ChatRoomsResponse;
 import dev.be.learnable.core.service.ChatRoomService;
+import java.util.Collections;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -42,7 +49,7 @@ class ChatRoomControllerTest {
     @DisplayName("[POST] 채팅방 생성 성공 테스트")
     void createChatRoom_success() throws Exception {
         //given
-        ChatRoomRequest chatRoomRequest = ChatRoomRequest.of(1L, 1L, "test-title");
+        ChatRoomRequest chatRoomRequest = ChatRoomRequest.of(1L, 1L, 1L, "test-title");
         BaseResponse<Void> baseResponse = new BaseResponse<>(CREATE_CHAT_ROOM_SUCCESS, null);
         willDoNothing().given(chatRoomService).create(any(ChatRoomDto.class));
 
@@ -60,7 +67,28 @@ class ChatRoomControllerTest {
         then(chatRoomService).should().create(any(ChatRoomDto.class));
     }
 
+    @Test
+    @DisplayName("[GET] 채팅방 전체 리스트 조회 성공 테스트")
+    void findAllChatRoom_success() throws Exception {
+        //given
+        ChatRoomsResponse response = ChatRoomsResponse.of(Collections.emptyList());
+        BaseResponse<ChatRoomsResponse> baseResponse = new BaseResponse<>(FIND_ALL_CHAT_ROOM_SUCCESS, response);
+        given(chatRoomService.findAll(anyLong())).willReturn(Collections.emptyList());
 
+        //when & then
+        mvc.perform(get("/chatrooms?memberId=1")
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(status().isOk())
+            .andExpect(content().string(objectMapper.writeValueAsString(baseResponse)));
+
+        then(chatRoomService).should().findAll(anyLong());
+    }
+
+
+    /**
+     * 한글 깨짐 현상 해결 작업
+     */
     @BeforeEach
     void init(WebApplicationContext wc) {
         mvc = MockMvcBuilders
