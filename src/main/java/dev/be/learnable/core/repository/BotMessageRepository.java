54ad894fb80893f8dc708cc57e2f83573b3a5e9f
DoooongDong <rhkswls98@kakao.com>
2023-04-01 03:19:26 +0900

    feature: [#15] 채팅방에 존재하는 봇, 유저 메시지 전체 조회

--- src/main/java/dev/be/learnable/core/repository/BotMessageRepository.java
+++ src/main/java/dev/be/learnable/core/repository/BotMessageRepository.java
@@ -1,8 +1,9 @@
 package dev.be.learnable.core.repository;
 
 import dev.be.learnable.core.domain.BotMessage;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BotMessageRepository extends JpaRepository<BotMessage, Long> {
-
+    List<BotMessage> findBotMessageByChatRoom_Id(Long chatroomId);
 }
--- src/main/java/dev/be/learnable/core/repository/UserMessageRepository.java
+++ src/main/java/dev/be/learnable/core/repository/UserMessageRepository.java
@@ -1,8 +1,9 @@
 package dev.be.learnable.core.repository;
 
 import dev.be.learnable.core.domain.UserMessage;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserMessageRepository extends JpaRepository<UserMessage, Long> {
-
+    List<UserMessage> findUserMessageByChatRoom_Id(Long chatroomId);
 }
