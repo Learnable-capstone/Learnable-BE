a46b0d1acea4faf8c672dde9968402e4e0df8fa4
DoooongDong <rhkswls98@kakao.com>
2023-04-01 03:18:21 +0900

    feature: [#15] 채팅방 성공 메시지 추가

--- src/main/java/dev/be/learnable/common/response/ResponseCodeAndMessages.java
+++ src/main/java/dev/be/learnable/common/response/ResponseCodeAndMessages.java
@@ -16,6 +16,8 @@ public enum ResponseCodeAndMessages implements CodeAndMessages {
      */
     CREATE_CHAT_ROOM_SUCCESS("S-CR001", "채팅방 생성에 성공했습니다."),
     FIND_ALL_CHAT_ROOM_SUCCESS("S-CR002", "채팅방 전체 조회에 성공했습니다."),
+    FIND_ONE_CHAT_ROOM_SUCCESS("S-CR003", "채팅방 단일 조회에 성공했습니다."),
+    DELETE_CHAT_ROOM_SUCCESS("S-CR004", "채팅방 삭제에 성공했습니다."),
     ;
 
     private final String code;
