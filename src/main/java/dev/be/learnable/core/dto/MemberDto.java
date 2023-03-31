84c93f5e815ac69533856ff6c9535c1248d1e099
Jung Ji Won <abiding77@naver.com>
2023-03-31 17:34:34 +0900

    feature: [#1] Member DTO 변경

--- src/main/java/dev/be/learnable/core/dto/MemberDto.java
+++ src/main/java/dev/be/learnable/core/dto/MemberDto.java
@@ -1,32 +1,31 @@
 package dev.be.learnable.core.dto;
 
 import dev.be.learnable.core.domain.Member;
-import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @RequiredArgsConstructor
 public class MemberDto {
     private final String username;
-    private final String password;
     private final String email;
     private final String role;
-    private final String social_type;
-    private final String social_id;
+    private final String socialtype;
+    private final String socialid;
     private final LocalDateTime createdAt;
     private final LocalDateTime modifiedAt;
 
     public static MemberDto from(Member member) {
         return new MemberDto(
             member.getUsername(),
-            member.getPassword(),
             member.getEmail(),
             member.getRole(),
-            member.getSocial_type(),
-            member.getSocial_id(),
+            member.getSocialtype(),
+            member.getSocialid(),
             member.getCreatedAt(),
             member.getUpdatedAt()
         );
