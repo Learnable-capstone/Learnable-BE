c345a149acd062eb7fcd0392afc70e1b48fc9f1e
Jung Ji Won <abiding77@naver.com>
2023-05-26 15:16:48 +0900

    feat: [#35] JPA 채팅방 번호로 최근 정답 조회 추가

--- src/main/java/dev/be/learnable/core/repository/BotMessageRepository.java
+++ src/main/java/dev/be/learnable/core/repository/BotMessageRepository.java
@@ -2,8 +2,16 @@ package dev.be.learnable.core.repository;
 
 import dev.be.learnable.core.domain.BotMessage;
 import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BotMessageRepository extends JpaRepository<BotMessage, Long> {
     List<BotMessage> findBotMessageByChatRoom_Id(Long chatroomId);
+
+    @Query("SELECT b.answer FROM BotMessage b WHERE b.chatRoom.id = :chatRoomId ORDER BY b.id DESC")
+    List<String> findLatestAnswerByChatRoomId(@Param("chatRoomId") Long chatRoomId, Pageable pageable);
+
 }
